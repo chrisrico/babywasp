eca91a786df15aecb64c9c5622d686eee9c929ba
Chris Rico <chrisrico@gmail.com>
2016-12-23 17:29:10 -0600

    Fix time zone bug in babywasp-start Fix bug causing message to not be recorded

--- web/middleware/babywasp.js
+++ web/middleware/babywasp.js
@@ -1,12 +1,11 @@
 module.exports = function (req, res, next) {
     req.password = req.get('x-api-key');
-    console.log(req.password);
     req.body = {
         outputs: [{
             toAddress: req.params.address,
             amount: parseInt(req.query.amount)
         }],
-        message: req.query.message
+        message: req.query.description
     };
     next();
 };
